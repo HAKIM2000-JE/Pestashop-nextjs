e6c6093f0ec6f6976b10e1739e6eeb2b6434f511
HAKIM2000-JE <abdelhakimjebabra7@gmail.com>
2021-11-28 21:11:16 +0100

    remove crossOrigin

--- maison-asa/.next/static/chunks/webpack.js
+++ maison-asa/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4b72728a171077f4"; }
+/******/ 		__webpack_require__.h = function() { return "fa4ee5ac8c1cbd68"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- maison-asa/pages/index.js
+++ maison-asa/pages/index.js
@@ -14,7 +14,21 @@ export default function Home() {
     
     <div className={styles.container_app}>
     <Head>
-  
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+  crossorigin="anonymous"
+/>
+    <script src="https://unpkg.com/react/umd/react.production.min.js" ></script>
+
+<script
+  src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+  crossorigin></script>
+
+<script
+  src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+  crossorigin></script>
   <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=320, height=device-height, target-densitydpi=medium-dpi" />
       </Head>
       
